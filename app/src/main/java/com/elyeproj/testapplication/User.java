09b6bccdec500c601f8e42da724299d109ebc712
Elisha Lye <elisha.lye@outware.com.au>
2016-03-30 14:45:13 +1100

    Put in change as per done setting the app.

---
+++ app/src/main/java/com/elyeproj/testapplication/User.java
@@ -0,0 +1,16 @@
+package com.elyeproj.testapplication;
+
+/**
+ * Created by elishalye on 30/03/2016.
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/com/elyeproj/testapplication/UserTest.java
@@ -0,0 +1,30 @@
+package com.elyeproj.testapplication;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by elishalye on 30/03/2016.
+ */
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class UserTest {
+
+    @Test
+    public void UserNameTest() {
+        User u = new User();
+
+        u.setName("foo");
+
+        Assert.assertEquals("foo", u.getName());
+    }
+
+    @Test
+    public void failingTest() {
+        Assert.assertFalse(true);
+    }
+}
